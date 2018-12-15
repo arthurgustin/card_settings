580e14292de4942ce5eaa5ca9b9e8d92310f6649
Jonathan White <ThinkDigitalRepair@gmail.com>
2018-12-14 23:38:52 -0800

    feat: add underline under dropdown menu (#10)

--- lib/widgets/card_settings_field.dart
+++ lib/widgets/card_settings_field.dart
@@ -4,7 +4,7 @@
 import 'package:card_settings/card_settings.dart';
 import 'package:flutter/material.dart';
 
-/// This is the basic layout of a field in a CardSettings view. Typcially, it
+/// This is the basic layout of a field in a CardSettings view. Typically, it
 /// will not be used directly.
 class CardSettingsField extends StatelessWidget {
   CardSettingsField({
@@ -37,27 +37,22 @@ class CardSettingsField extends StatelessWidget {
   Widget build(BuildContext context) {
     return (visible)
         ? Container(
-            decoration: BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-                      width: 1.0, color: Theme.of(context).dividerColor)),
-            ),
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    _buildLabelRow(context),
-                    _buildInlineContent(context),
-                    _buildRightDecoration()
-                  ],
-                ),
-                _buildRowContent()
-              ],
-            ),
-          )
+      padding: EdgeInsets.all(14.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildLabelRow(context),
+              _buildInlineContent(context),
+              _buildRightDecoration()
+            ],
+          ),
+          _buildRowContent()
+        ],
+      ),
+    )
         : Container();
   }
 
@@ -70,9 +65,9 @@ class CardSettingsField extends StatelessWidget {
       final InputDecoration decoration = const InputDecoration()
           .applyDefaults(Theme.of(context).inputDecorationTheme)
           .copyWith(
-              errorText: errorText,
-              contentPadding: EdgeInsets.all(0.0),
-              border: InputBorder.none);
+          errorText: errorText,
+          contentPadding: EdgeInsets.only(bottom: 3),
+          border: UnderlineInputBorder());
 
       decoratedContent = InputDecorator(decoration: decoration, child: content);
     }
@@ -83,9 +78,9 @@ class CardSettingsField extends StatelessWidget {
   Widget _buildRowContent() {
     return (contentOnNewLine)
         ? Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: content,
-          )
+      padding: EdgeInsets.only(top: 10.0),
+      child: content,
+    )
         : Container();
   }
 
@@ -93,7 +88,7 @@ class CardSettingsField extends StatelessWidget {
     return Container(
       width: (contentOnNewLine) ? null : labelWidth,
       padding:
-          EdgeInsets.only(right: CardSettings.of(context).labelPadding ?? 6.0),
+      EdgeInsets.only(right: CardSettings.of(context).labelPadding ?? 6.0),
       child: Row(
         children: <Widget>[
           _buildLeftIcon(context),
@@ -115,9 +110,9 @@ class CardSettingsField extends StatelessWidget {
 
   Widget _buildLabelSpacer(BuildContext context) {
     return ((labelAlign ??
-                CardSettings.of(context).labelAlign ??
-                TextAlign.left) ==
-            TextAlign.right)
+        CardSettings.of(context).labelAlign ??
+        TextAlign.left) ==
+        TextAlign.right)
         ? Expanded(child: Container())
         : Container();
   }
@@ -149,7 +144,7 @@ class CardSettingsField extends StatelessWidget {
 
   TextStyle _buildLabelStyle(BuildContext context) {
     TextStyle labelStyle = TextStyle(
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeight.normal,
       fontSize: 16.0,
     );
 
@@ -160,26 +155,26 @@ class CardSettingsField extends StatelessWidget {
     return (icon == null)
         ? Container()
         : Container(
-            margin: EdgeInsets.all(0.0),
-            padding: EdgeInsets.only(right: 4.0),
-            child: Icon(
-              icon.icon,
-              color: Theme.of(context).inputDecorationTheme.labelStyle.color,
-            ),
-          );
+      margin: EdgeInsets.all(0.0),
+      padding: EdgeInsets.only(right: 4.0),
+      child: Icon(
+        icon.icon,
+        color: Theme.of(context).inputDecorationTheme.labelStyle.color,
+      ),
+    );
   }
 
   Widget _buildRightDecoration() {
     return (pickerIcon != null || unitLabel != null)
         ? Container(
-            alignment: Alignment.centerRight,
-            child: (pickerIcon != null)
-                ? Icon(pickerIcon)
-                : Text(
-                    unitLabel,
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-          )
+      alignment: Alignment.centerRight,
+      child: (pickerIcon != null)
+          ? Icon(pickerIcon)
+          : Text(
+        unitLabel,
+        style: TextStyle(fontStyle: FontStyle.italic),
+      ),
+    )
         : Container();
   }
 }
